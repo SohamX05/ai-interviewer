369b1ff76225bd84e1516cc11486d33b3e6fd4ef
Soham Mondal <mondalsoham84@gmail.com>
2026-02-21 05:12:18 +0530

    Feature: Streamlit UI and Llama 3.1 model.

--- interviewer.py
+++ interviewer.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from groq import Groq
 
@@ -8,7 +9,7 @@ api_key = os.getenv("GROK_API_KEY")
 
 client = Groq(api_key=api_key) #initialize the client
 
-def get_ai_feedback(question, user_answer): #set up conversation history and protocols for the AI Interviewer
+def get_ai_feedback(question, user_answer): #setting up conversation history and protocols for the AI Interviewer
     messages = [
         {
             "role": "system",
@@ -21,10 +22,31 @@ def get_ai_feedback(question, user_answer): #set up conversation history and pro
     ]
     #response from AI Interviewer
     response = client.chat.completions.create(
-        model="llama3-8b--8192",
+        model="llama-3.1-8b-instant",
         messages=messages,
         max_tokens=200
     )
     
-    #return the feedback
+    #returns the feedback
     return response.choices[0].message.content
+
+#Streamlit UI
+st.title("AI Interviewer")
+st.divider()
+
+#setting up the question
+question = "Can you explain the difference between an Interface and an Abstract Class in Java?"
+st.markdown(f"**Interviewer**: {question}")
+
+#Creates a text box for user to answer
+user_answer = st.text_area("Your Answer: ", height=150)
+
+#Creating a submit button
+if st.button("Submit Answer"):
+    if user_answer: #checks if typed or not
+        with st.spinner("Analyzing your answer..."):
+            feedback = get_ai_feedback(question, user_answer) #Calls the feedback funstion
+            st.subheader("**Feedback**:")
+            st.write(feedback) #Displays the feedback
+    else:
+        st.warning("Please enter your answer before submitting!")
